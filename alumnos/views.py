a7fe31d19f85da79c73179c3fd08492dcea4ced4
HerreraHernandezJovannaJatziry <20223tn062@utez.edu.mx>
2025-03-16 19:29:04 -0600

    alumnos y nav

--- alumnos/views.py
+++ alumnos/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .views import *
+from django.urls import path,include
 
 # Create your views here.
 def ver_asistencia(request):
--- attendanceControl/urls.py
+++ attendanceControl/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
 from django.urls import include
 
 from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('alumnos/', include('alumnos.urls')),
     
 ]
