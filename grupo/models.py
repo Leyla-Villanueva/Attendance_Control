f55d78c14d204b7da1b3cee0652b42f0ffc01686
AlanYamami <20223TN046@utez.edu.mx>
2025-03-31 01:24:24 -0600

    crud grupos

--- grupo/models.py
+++ grupo/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Grupo(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
+    grado = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre
--- grupo/serializers.py
+++ grupo/serializers.py
@@ -1,8 +1,55 @@
 from rest_framework import serializers
 from .models import Grupo
+from alumnos.models import Alumno  # Importa el modelo Alumno
+from carrera.models import Carrera  # Importa el modelo Carrera
+
 
 class GrupoSerializer(serializers.ModelSerializer):
+    alumnos = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Alumno.objects.all(), required=False
+    )  # Campo para recibir los IDs de los alumnos
+    carrera = serializers.PrimaryKeyRelatedField(
+        queryset=Carrera.objects.all(), required=False
+    )  # Campo para recibir el ID de la carrera
+
     class Meta:
         model = Grupo
-        fields = '__all__'
+        fields = "__all__"
 
+    def create(self, validated_data):
+        # Extraer los IDs de los alumnos y la carrera del JSON
+        alumnos_data = validated_data.pop("alumnos", [])
+        carrera_data = validated_data.pop("carrera", None)
+
+        # Crear el grupo
+        grupo = Grupo.objects.create(**validated_data)
+
+        # Asignar el grupo, grado y carrera a los alumnos especificados
+        for alumno in alumnos_data:
+            alumno.grupo = grupo
+            alumno.grado = grupo.grado
+            if carrera_data:  # Si se especifica una carrera, actualizarla
+                alumno.carrera_id = carrera_data
+            alumno.save()
+
+        return grupo
+
+    def update(self, instance, validated_data):
+        # Extraer los IDs de los alumnos y la carrera del JSON
+        alumnos_data = validated_data.pop("alumnos", [])
+        carrera_data = validated_data.pop("carrera", None)
+
+        # Actualizar el grupo
+        instance.nombre = validated_data.get("nombre", instance.nombre)
+        instance.grado = validated_data.get("grado", instance.grado)
+        instance.save()
+
+        # Asignar el grupo, grado y carrera a los alumnos especificados
+        for alumno in alumnos_data:
+            alumno.grupo = instance
+            alumno.grado = instance.grado
+            if carrera_data:  # Si se especifica una carrera, actualizarla
+                alumno.carrera_id = carrera_data
+            alumno.save()
+
+        return instance
