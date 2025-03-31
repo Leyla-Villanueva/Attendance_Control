06a88a58b0c0ebc3922a713f207d4965634ad40e
AlanYamami <20223TN046@utez.edu.mx>
2025-03-30 22:02:30 -0600

    Resuelto conflicto en urls.py

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-from django.urls import path
-from .views import *
-from rest_framework.routers import SimpleRouter
-
-router = SimpleRouter()
-router.register('api', AlumnoViewSet)
-
-urlpatterns = [
-    path("ver/", ver_asistencia, name="ver"),
-    path('', include(router.urls))
-]
-=======
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AlumnoViewSet
+from .views import AlumnoViewSet, ver_asistencia
 
+# Usamos DefaultRouter para registrar las rutas
 router = DefaultRouter()
-router.register(r'alumnos', AlumnoViewSet, basename='alumno')
+router.register(r"alumnos", AlumnoViewSet, basename="alumno")
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path("ver/", ver_asistencia, name="ver"),  # Incluimos la ruta para ver asistencia
+    path("api/", include(router.urls)),  # Incluimos las rutas del router
 ]
->>>>>>> Ana-Jatz
