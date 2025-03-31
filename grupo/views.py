1a337025e15e101490554365d698f9c002aa559f
AlanYamami <20223TN046@utez.edu.mx>
2025-03-31 01:26:20 -0600

    crud grupos

--- grupo/views.py
+++ grupo/views.py
@@ -3,12 +3,9 @@ from rest_framework import viewsets
 from .models import Grupo
 from .serializers import GrupoSerializer
 
+
 class GrupoViewSet(viewsets.ModelViewSet):
-
     queryset = Grupo.objects.all()
-
     serializer_class = GrupoSerializer
-
     renderer_classes = [JSONRenderer]
-    
-    http_method_names = ['get', 'post', 'put', 'delete']
+    http_method_names = ["get", "post", "put", "delete"]
