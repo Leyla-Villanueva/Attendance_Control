dc5b2f4102280af857ac098f7787e3747528dc8a
AlanYamami <20223TN046@utez.edu.mx>
2025-03-30 22:03:50 -0600

    Resuelto conflicto en views.py

--- alumnos/views.py
+++ alumnos/views.py
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
 from django.shortcuts import render
-from .views import *
-from django.urls import path,include
-=======
 from rest_framework.renderers import JSONRenderer
->>>>>>> Ana-Jatz
 from rest_framework import viewsets
 from .models import Alumno
 from .serializers import AlumnoSerializer
 
-<<<<<<< HEAD
-# Create your views here.
+# Vista para ver asistencia
 def ver_asistencia(request):
     return render(request, 'inicio.html')
 
 class AlumnoViewSet(viewsets.ModelViewSet):
-    queryset = Alumno.objects.all()
-    serializer_class = AlumnoSerializer
-    
-=======
-class AlumnoViewSet(viewsets.ModelViewSet):
-
     # Esta variable indica de dónde sacar el modelo y la información de la BD
     queryset = Alumno.objects.all()
     
@@ -32,4 +20,3 @@ class AlumnoViewSet(viewsets.ModelViewSet):
     
     # Permitir filtrar qué métodos HTTP se pueden usar
     http_method_names = ['get', 'post', 'put', 'delete']
->>>>>>> Ana-Jatz
