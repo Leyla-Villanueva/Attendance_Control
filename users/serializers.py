6edfc11d57a17570ca3f4edc50a529dcf98fafbc
AlanYamami <20223TN046@utez.edu.mx>
2025-04-03 02:36:48 -0600

    fix/mas datos para poder usarlos en sidebar de perfil

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import User
 from rest_framework import serializers
+from alumnos.models import Alumno
+from maestros.models import Maestro
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -12,6 +14,28 @@ class CustomUserSerializer(serializers.ModelSerializer):
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        # Agregar el rol del usuario al token de respuesta
+
+        # Agregar el rol del usuario
         data["role"] = self.user.rol.rol  # Usar el campo 'rol' del modelo Rol
+
+        # Agregar el nombre de usuario
+        data["username"] = self.user.username
+
+        # Verificar si el usuario es un Alumno
+        if hasattr(self.user, "alumno"):
+            alumno = self.user.alumno
+            data["nombre"] = alumno.nombre
+            data["apellido_paterno"] = alumno.apellido_paterno
+            data["apellido_materno"] = alumno.apellido_materno
+            data["grado"] = alumno.grado
+            data["grupo"] = alumno.grupo.nombre if alumno.grupo else None
+            data["carrera"] = alumno.carrera_id.nombre if alumno.carrera_id else None
+
+        # Verificar si el usuario es un Maestro
+        elif hasattr(self.user, "maestro"):
+            maestro = self.user.maestro
+            data["nombre"] = maestro.nombre
+            data["apellido_paterno"] = maestro.apellido_paterno
+            data["apellido_materno"] = maestro.apellido_materno
+
         return data
