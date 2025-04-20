f912a608ca30ee64ef4777e602bfe3988b0cc4a9
HerreraHernandezJovannaJatziry <20223tn062@utez.edu.mx>
2025-04-20 16:55:12 -0600

    alumnos con asistencia

--- asistencia/urls.py
+++ asistencia/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AsistenciaViewSet
+from .views import AsistenciaViewSet, calcular_porcentaje_asistencia, calcular_porcentaje_asistencia_general
 
 router = DefaultRouter()
 router.register(r'api', AsistenciaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('calcular-porcentaje/',calcular_porcentaje_asistencia, name='calcular_porcentaje_asistencia'),
+    path('porcentaje_general/',calcular_porcentaje_asistencia_general, name='calcular_porcentaje_asistencia_general')
 ]
--- asistencia/views.py
+++ asistencia/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Asistencia, Unidad
 from .serializers import AsistenciaSerializer
+from django.db import connection  
+from rest_framework.decorators import api_view  
+from rest_framework.response import Response
 
 class AsistenciaViewSet(viewsets.ModelViewSet):
     queryset = Asistencia.objects.all()
@@ -48,3 +51,30 @@ class AsistenciaViewSet(viewsets.ModelViewSet):
             )
 
         return Response({"message": "Asistencias registradas o actualizadas correctamente"}, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def calcular_porcentaje_asistencia (request):
+    alumno_id =request.query_params.get('alumno_id')
+    clase_id = request.query_params.get('clase_id')
+    if not alumno_id or not clase_id:
+        return Response({"error": "Faltan parámetros requeridos"}, status=400)
+    with connection.cursor() as cursor:
+        cursor.callproc("calcular_porcentaje_asistencia", [alumno_id, clase_id, 0])
+        cursor.execute("SELECT @`_calcular_porcentaje_asistencia_2`;")
+        resultado = cursor.fetchone()[0]
+
+    return Response({"porcentaje": resultado})
+    
+@api_view(['GET'])
+def calcular_porcentaje_asistencia_general(request):
+    alumno_id = request.query_params.get('alumno_id')
+    clase_id = request.query_params.get('clase_id')
+    if not alumno_id or not clase_id:
+        return Response({"error": "Faltan parámetros requeridos"}, status=400)
+    with connection.cursor() as cursor:
+        cursor.callproc("calcular_porcentaje_asistencia_general", [alumno_id, clase_id, 0])
+        cursor.execute("SELECT @`_calcular_porcentaje_asistencia_general_2`;")
+        resultado = cursor.fetchone()[0]
+
+    return Response({"porcentaje_general": resultado})
+
